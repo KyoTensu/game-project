cc3beae8c23a6806fc54cb01f184b293953362f1
Edmaruu <92155020+Edmaruu@users.noreply.github.com>
2023-06-12 14:53:26 +0200

    Ajout d'un script pour bouger des cubes
    Possibilité de pousser les cubes qu'on a choisi comme étant déplaçable

---
+++ Assets/cube_move_debug.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_move_debug : MonoBehaviour
+{
+    public Rigidbody cube;
+
+    //Touche pour activer le deplacement du cube
+    public string inputPush = "j";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col) // Recuperer une collision
+    {
+        if (col.gameObject.name == "Bloc_movable" && Input.GetKey(inputPush))
+        {
+            cube.isKinematic = false;// active le rigidbody 
+        }
+
+         if (col.gameObject.name == "Bloc_movable" && !Input.GetKey(inputPush))
+        {
+            cube.isKinematic = true;// desactive le rigidbody 
+        }
+
+    }
+
+    void update ()
+    {
+   
+    }
+    
+}
