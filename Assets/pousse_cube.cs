f3a568811422b3e0e34e16698036a3a671226461
Edmaruu <92155020+Edmaruu@users.noreply.github.com>
2023-06-20 22:03:44 +0200

    ajout d'un cube déplaçable

--- Assets/pousse_cube.cs
+++ Assets/pousse_cube.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class pousse_cube : MonoBehaviour
 {
     public Rigidbody cube;
+    public Rigidbody cube2;
 
     //Touche pour activer le deplacement du cube
     public string inputPush = "j";
@@ -20,7 +21,7 @@ public class pousse_cube : MonoBehaviour
         if (col.gameObject.name == "box_low (1)" && Input.GetKey(inputPush))
         {
             cube.isKinematic = false;// active le rigidbody 
-            print("ça marche");
+            
         }
 
         if (col.gameObject.name == "box_low (1)" && !Input.GetKey(inputPush))
@@ -28,6 +29,16 @@ public class pousse_cube : MonoBehaviour
             cube.isKinematic = true;// desactive le rigidbody 
         }
 
+        if (col.gameObject.name == "CarboardBox_1 (18)" && Input.GetKey(inputPush))
+        {
+            cube2.isKinematic = false;// active le rigidbody 
+            
+        }
+
+        if (col.gameObject.name == "CarboardBox_1 (18)" && !Input.GetKey(inputPush))
+        {
+            cube2.isKinematic = true;// desactive le rigidbody 
+        }
     }
 
     void update()
